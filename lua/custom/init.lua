2eca630d35af71a3d90c68f86f342c5c87af8f82
Grandkahuna43325 <kf.korulczyk@gmail.com>
2023-06-26 18:44:13 +0200

    changed back to wrap

--- lua/custom/init.lua
+++ lua/custom/init.lua
@@ -2,11 +2,10 @@ vim.opt.nu = true
 vim.opt.relativenumber = true
 vim.opt.scrolloff = 6
 vim.cmd([[ 
-set nowrap
+" set nowrap
 set linebreak
 
 ]])
-
 vim.g.markdown_fenced_languages = {
   "ts=typescript"
 }
