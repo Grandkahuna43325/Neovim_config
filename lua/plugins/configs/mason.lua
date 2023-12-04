4994731c6199a1ab7cd8ff1e680a5f6664f4a9b5
Grandkahuna43325 <kf.korulczyk@gmail.com>
2023-12-04 11:21:36 +0100

    added clippy to rust

--- lua/plugins/configs/mason.lua
+++ lua/plugins/configs/mason.lua
@@ -1,48 +1,48 @@
-local capabilities = require('cmp_nvim_lsp').default_capabilities()
+local capabilities = require("cmp_nvim_lsp").default_capabilities()
 local lspconfig = require("lspconfig")
-local lsp_zero = require('lsp-zero')
-require('mason').setup({})
-require('mason-lspconfig').setup({
-  ensure_installed = {'tsserver', 'rust_analyzer', 'lua_ls'},
-  handlers = {
-    lsp_zero.default_setup,
-    lua_ls = function()
-      local lua_opts = lsp_zero.nvim_lua_ls()
-      require('lspconfig').lua_ls.setup(lua_opts)
-    end,
-    rust_analyzer = function ()
-      local lua_opts = lsp_zero.nvim_lua_ls()
-	lspconfig.rust_analyzer.setup({
-		root_dir = function()
-			return vim.loop.cwd()
+local lsp_zero = require("lsp-zero")
+require("mason").setup({})
+require("mason-lspconfig").setup({
+	ensure_installed = { "tsserver", "rust_analyzer", "lua_ls" },
+	handlers = {
+		lsp_zero.default_setup,
+		lua_ls = function()
+			local lua_opts = lsp_zero.nvim_lua_ls()
+			require("lspconfig").lua_ls.setup(lua_opts)
 		end,
-		capabilities = capabilities,
-		on_attach = function(client, bufnr)
+		rust_analyzer = function()
+			local lua_opts = lsp_zero.nvim_lua_ls()
+			lspconfig.rust_analyzer.setup({
+				root_dir = function()
+					return vim.loop.cwd()
+				end,
+				capabilities = capabilities,
+				on_attach = function(client, bufnr) end,
+				settings = {
+					["rust-analyzer"] = {
+						checkOnSave = {
+							command = "clippy",
+						},
+						imports = {
+							granularity = {
+								group = "module",
+							},
+							prefix = "self",
+						},
+						cargo = {
+							buildScripts = {
+								enable = true,
+							},
+						},
+						procMacro = {
+							enable = true,
+						},
+					},
+				},
+			})
 		end,
-		settings = {
-			["rust-analyzer"] = {
-			imports = {
-				granularity = {
-					group = "module",
-				},
-				prefix = "self",
-			},
-			cargo = {
-				buildScripts = {
-					enable = true,
-				},
-			},
-			procMacro = {
-				enable = true
-			},
-		}
-	}
+	},
 })
-    end,
-  }
-})
-
-
 
 local settings = {
 	ensure_installed = { "lua-language-server" }, -- not an option from mason.nvim
@@ -58,8 +58,7 @@ local settings = {
 			uninstall_server = "X",
 			cancel_installation = "<C-c>",
 		},
-	}
-
+	},
 }
 
 return settings
