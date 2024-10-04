1e74c57ebc8eba16158b4967c66d23dcc63ab8a5
grandkahuna43325 <kf.korulczyk@gmail.com>
2024-10-04 19:34:08 +0200

    commented out neorg and useless config

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -77,11 +77,11 @@ lazy.setup({
     -- },
     {
         "rcarriga/nvim-notify",
-        config = function()
-            require("notify").setup({
-                background_colour = "#000000",
-            })
-        end,
+        -- config = function()
+            -- require("notify").setup({
+            --     background_colour = "#000000",
+            -- })
+        -- end,
     },
     {
         "ThePrimeagen/vim-apm",
@@ -532,26 +532,26 @@ lazy.setup({
         priority = 1000, -- We'd like this plugin to load first out of the rest
         config = true, -- This automatically runs `require("luarocks-nvim").setup()`
     },
-    {
-        "nvim-neorg/neorg",
-        build = ":Neorg sync-parsers",
-        dependencies = { "nvim-lua/plenary.nvim", "luarocks.nvim" },
-        config = function()
-            require("neorg").setup({
-                load = {
-                    ["core.defaults"] = {}, -- Loads default behaviour
-                    ["core.concealer"] = {}, -- Adds pretty icons to your documents
-                    ["core.dirman"] = { -- Manages Neorg workspaces
-                        config = {
-                            workspaces = {
-                                notes = "~/notes",
-                            },
-                        },
-                    },
-                },
-            })
-        end,
-    },
+    -- {
+    --     "nvim-neorg/neorg",
+    --     build = ":Neorg sync-parsers",
+    --     dependencies = { "nvim-lua/plenary.nvim", "luarocks.nvim" },
+    --     config = function()
+    --         require("neorg").setup({
+    --             load = {
+    --                 ["core.defaults"] = {}, -- Loads default behaviour
+    --                 ["core.concealer"] = {}, -- Adds pretty icons to your documents
+    --                 ["core.dirman"] = { -- Manages Neorg workspaces
+    --                     config = {
+    --                         workspaces = {
+    --                             notes = "~/notes",
+    --                         },
+    --                     },
+    --                 },
+    --             },
+    --         })
+    --     end,
+    -- },
 
     -- {
     --     "Exafunction/codeium.nvim",
